dd6ff75fb5d24ee9445534ddc73b4d82b2a43ec3
Roberth Karman <rokarman@microsoft.com>
2021-02-17 14:41:38 -0800

    Update to main
    * Added ApiInformation check to verify that we can actually do multi-instancing. Falling back to the normal XAML auto generated main if not.

--- src/Calculator/Program.cpp
+++ src/Calculator/Program.cpp
@@ -24,71 +24,85 @@ int __cdecl main(::Platform::Array<::Platform::String ^> ^ args)
 {
     (void)args; // Unused parameter.
 
-    // In some scenarios, the platform might indicate a recommended instance.
-    // If so, we can redirect this activation to that instance instead, if we wish.
-    if (AppInstance::RecommendedInstance != nullptr)
+    //Check to see if we are on a version that supports the AppInstance APIs.
+    if (Windows::Foundation::Metadata::ApiInformation::IsTypePresent("Windows.ApplicationModel.Activation.AppInstance"))
     {
-        AppInstance::RecommendedInstance->RedirectActivationTo();
-    }
-    else
-    {
-        // Try to open the mutex we use to signal that we're on a phone style device (or in tablet mode)
-        HANDLE hInstance = OpenMutexW(SYNCHRONIZE, NULL, L"SingleInstanceCalculator");
-
-        // Was the mutex created?
-        if (hInstance != NULL)
+        // In some scenarios, the platform might indicate a recommended instance.
+        // If so, we can redirect this activation to that instance instead, if we wish.
+        if (AppInstance::RecommendedInstance != nullptr)
         {
-            // On non-Phone style device we will always launch a new instance
-            // So we will go ahead and reuse the first one we find. :)
-
-            // First release the mutex, we don't need it anymore
-            ReleaseMutex(hInstance);
-
-            // Double check to see that the instance is still around...
-            if (AppInstance::GetInstances()->Size > 0)
-            {
-                // Then go ahead and redirect to the first instance in the list
-                AppInstance::GetInstances()->GetAt(0)->RedirectActivationTo();
-            }
-            else
-            {
-                // ("if(The thing that shouldn't happen actually happened)" ;) )
-                // The old instance exited before we got this far, so go ahead and createa a new instance after all.
-                AppInstance ^ instance = AppInstance::FindOrRegisterInstanceForKey(CryptographicBuffer::GenerateRandomNumber().ToString());
-
-                if (instance->IsCurrentInstance)
-                {
-                    // If we successfully registered this instance, we can now just
-                    // go ahead and do normal XAML initialization.
-                    ::Windows::UI::Xaml::Application::Start(
-                        ref new ::Windows::UI::Xaml::ApplicationInitializationCallback([](::Windows::UI::Xaml::ApplicationInitializationCallbackParams ^ p) {
-                            (void)p; // Unused parameter.
-                            auto app = ref new ::CalculatorApp::App();
-                        }));
-                }
-            }
+            AppInstance::RecommendedInstance->RedirectActivationTo();
         }
         else
         {
-            // No mutex had been created by any other instance.
-            // This either means we're the first, or that we're on a device where we should multi-instance.
-            // So we go ahead and create a new instance either way.
-            AppInstance ^ instance = AppInstance::FindOrRegisterInstanceForKey(CryptographicBuffer::GenerateRandomNumber().ToString());
+            // Try to open the mutex we use to signal that we're on a phone style device (or in tablet mode)
+            HANDLE hInstance = OpenMutexW(SYNCHRONIZE, NULL, L"SingleInstanceCalculator");
 
-            // If the instace we got for the random number isn't us, we'll do this again...
-            while (!instance->IsCurrentInstance)
+            // Was the mutex created?
+            if (hInstance != NULL)
             {
-                // That random number was already in use. Find another.
-                instance = AppInstance::FindOrRegisterInstanceForKey(CryptographicBuffer::GenerateRandomNumber().ToString());
+                // On non-Phone style device we will always launch a new instance
+                // So we will go ahead and reuse the first one we find. :)
+
+                // First release the mutex, we don't need it anymore
+                ReleaseMutex(hInstance);
+
+                // Double check to see that the instance is still around...
+                if (AppInstance::GetInstances()->Size > 0)
+                {
+                    // Then go ahead and redirect to the first instance in the list
+                    AppInstance::GetInstances()->GetAt(0)->RedirectActivationTo();
+                }
+                else
+                {
+                    // ("if(The thing that shouldn't happen actually happened)" ;) )
+                    // The old instance exited before we got this far, so go ahead and createa a new instance after all.
+                    AppInstance ^ instance = AppInstance::FindOrRegisterInstanceForKey(CryptographicBuffer::GenerateRandomNumber().ToString());
+
+                    if (instance->IsCurrentInstance)
+                    {
+                        // If we successfully registered this instance, we can now just
+                        // go ahead and do normal XAML initialization.
+                        ::Windows::UI::Xaml::Application::Start(
+                            ref new ::Windows::UI::Xaml::ApplicationInitializationCallback([](::Windows::UI::Xaml::ApplicationInitializationCallbackParams ^ p) {
+                                (void)p; // Unused parameter.
+                                auto app = ref new ::CalculatorApp::App();
+                            }));
+                    }
+                }
             }
-            // If we successfully registered this instance, we can now just
-            // go ahead and do normal XAML initialization.
-            ::Windows::UI::Xaml::Application::Start(
-                ref new ::Windows::UI::Xaml::ApplicationInitializationCallback([](::Windows::UI::Xaml::ApplicationInitializationCallbackParams ^ p) {
-                    (void)p; // Unused parameter.
-                    auto app = ref new ::CalculatorApp::App();
-                }));
+            else
+            {
+                // No mutex had been created by any other instance.
+                // This either means we're the first, or that we're on a device where we should multi-instance.
+                // So we go ahead and create a new instance either way.
+                AppInstance ^ instance = AppInstance::FindOrRegisterInstanceForKey(CryptographicBuffer::GenerateRandomNumber().ToString());
+
+                // If the instace we got for the random number isn't us, we'll do this again...
+                while (!instance->IsCurrentInstance)
+                {
+                    // That random number was already in use. Find another.
+                    instance = AppInstance::FindOrRegisterInstanceForKey(CryptographicBuffer::GenerateRandomNumber().ToString());
+                }
+                // If we successfully registered this instance, we can now just
+                // go ahead and do normal XAML initialization.
+                ::Windows::UI::Xaml::Application::Start(
+                    ref new ::Windows::UI::Xaml::ApplicationInitializationCallback([](::Windows::UI::Xaml::ApplicationInitializationCallbackParams ^ p) {
+                        (void)p; // Unused parameter.
+                        auto app = ref new ::CalculatorApp::App();
+                    }));
+            }
         }
+    }
+    else
+    {
+        // This version of Windows does not support multi instancing,
+        // just go ahead with the normal XAML initialization.
+        ::Windows::UI::Xaml::Application::Start(
+            ref new ::Windows::UI::Xaml::ApplicationInitializationCallback([](::Windows::UI::Xaml::ApplicationInitializationCallbackParams ^ p) {
+                (void)p; // Unused parameter.
+                auto app = ref new ::CalculatorApp::App();
+            }));
     }
     return 0;
 }
